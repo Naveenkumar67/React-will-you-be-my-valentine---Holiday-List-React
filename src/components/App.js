356fb65122f84be82699453c99fe6f0047755c7f
Naveenkumar67 <naveenvempati4@gmail.com>
2021-02-26 17:24:55 +0530

    klj

--- src/components/App.js
+++ src/components/App.js
@@ -7,45 +7,22 @@ class App extends Component {
     super(props)
     this.res=this.res.bind(this);
     this.arr=this.arr.bind(this);
+    this.list=this.list.bind(this);
     this.cityList = [{ name: 'Goa', country: 'India' },
     { name: 'Amsterdam', country: 'Netherlands' },
     { name: 'New York', country: 'USA' },
     { name: 'Darjeeling', country: 'India' },
     { name: 'Tokyo', country: 'Japan' },
     { name: 'Lonavala', country: 'India' },
-    { name: 'Brandenburg Gate', country: 'Germany' },
-    { name: 'Reichstag Building', country: 'Germany' },
-    { name: 'Museum Island', country: 'Germany' },
-    { name: 'Munnar', country: 'India' },
-    { name: 'Leh Ladakh', country: 'India' },
-    { name: 'Goa', country: 'India' },
-    { name: 'Agra', country: 'India' },
-    { name: 'Dalhousie', country: 'India' },
-    { name: 'Coorg', country: 'India' },
-    { name: 'Rome', country: 'Italy' },
-    { name: 'Milan', country: 'Italy' },
-    { name: 'Venice', country: 'Italy' },
-    { name: 'Varanasai', country: 'India' },
-    { name: 'Jaipur', country: 'India' },
-    { name: 'The Hofburg', country: 'Austria' },
-    { name: 'Belvedere Palace', country: 'Austria' },
-    { name: 'St. Stephen Cathedral', country: 'Austria' },
-    { name: 'Kahna National Park', country: 'India' },
-    { name: 'Amritsar', country: 'India' },
-    { name: 'Mussoorie', country: 'India' },
-    { name: 'Mount Abu', country: 'India' },
-    { name: 'Tirupati', country: 'India' },
     ]
+    this.arr=this.city.filter((item)=>
+      item.country==='India')
+    this.list=this.arr.map((item)=><li key={item.country}>{item.city}</li>)  
   }
-  res(){
-     this.arr=this.city.map((item)=>{
-       if(this.item.country==='India'){
-         return item
-       }
-     })
-     this.cityList=this.arr;
+  
      
-  }
+     
+    
 
   
   render() {
@@ -54,8 +31,8 @@ class App extends Component {
         {/* Do not remove the main div */}
         
       <ol>
-      {this.res()}
-    </ol>
+      {this.list}
+      </ol>
       </div>
     )
   }
